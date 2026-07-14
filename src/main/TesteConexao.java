1830e233f01c7885ac31634ff986917dc43fdc51
italo cecconi teixeira gomes <italocecconiteixeiragomes@gmail.com>
2026-07-13 22:13:16 -0300

    Associando o codigo java com o banco de dados postgres

---
+++ src/main/TesteConexao.java
@@ -0,0 +1,28 @@
+package main;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class TesteConexao {
+
+    public static void main(String[] args) {
+        
+        // 1. Definicao das rotas e das credenciais
+        String url = "jdbc:postgresql://localhost:5432/sistema_bancario"; 
+        String usuario = "postgres"; 
+        String senha = "30141670"; 
+        
+        // 2. Tentativa de conexao
+        try {
+            Connection conexao = DriverManager.getConnection(url, usuario, senha);
+            System.out.println("Conexao com o PostgreSQL realizada com sucesso!");
+            
+            // Fecha a conexão após o teste
+            conexao.close(); 
+            
+        } catch (SQLException e) {
+            System.out.println("Err");
+            e.printStackTrace(); // Isso imprime o erro 
+        }
+    }
+}
--- src/module-info.java
+++ src/module-info.java
@@ -5,4 +5,5 @@
  * 
  */
 module Banco {
+	requires java.sql;
 }
